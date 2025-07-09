981f424264d8d3cc143482cd5aa0f4c7f247e0c7
Kelvin Cipta <ciptakelvin@gmail.com>
2025-07-09 20:59:03 +0700

    Flash Effect

--- Assets/Scripts/StoryManager.cs
+++ Assets/Scripts/StoryManager.cs
@@ -1,17 +1,21 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class StoryManager : MonoBehaviour
 {
     public UnityEvent onStoryEnded;
     public GameObject canvas;
+    public RawImage character;
     public TextMeshProUGUI title;
     public TextMeshProUGUI body;
+    public GameObject flash;
 
     public string[] dialogues; // Array to hold dialogue strings
+    public Texture[] characters;
     public AudioClip[] dialogueVO; // Array to hold audio clips for each dialogue
-    public AudioClip[] dialogueEffect;
+    public string[] dialogueEffect; //normal,flash,shake
 
     public int currentDialogueIndex = 0; // Index to track the current dialogue
 
@@ -29,9 +33,22 @@ public class StoryManager : MonoBehaviour
         canvas.SetActive(true);
         string name = dialogues[currentDialogueIndex].Split(",")[0];
         string text = dialogues[currentDialogueIndex].Split(",")[1];
+        Texture characterTexture = characters[currentDialogueIndex];
+        string effect = dialogueEffect[currentDialogueIndex];
 
         title.text = name;
         body.text = text;
+        character.texture = characterTexture;
+
+        switch(effect)
+        {
+            case "flash":
+                flash.SetActive(true);
+                break;
+            default:
+                flash.SetActive(false);
+                break;
+        }
 
         currentDialogueIndex++;
     }
